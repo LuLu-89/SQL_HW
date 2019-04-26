d0a138cf8d4caec8aaf4794631909077645609d2
LuLu-89 <lucy.ly89@gmail.com>
2019-04-26 09:57:33 -0600

    Update SQL_HW.sql

--- SQL_HW.sql
+++ SQL_HW.sql
@@ -1,8 +1,8 @@
 use sakila;
 
 -- 1a. Display the first and last names of all actors from the table actor.
-SELECT first_name, last_name
- FROM actor;
+select first_name, last_name
+ from actor;
  
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 select upper(concat(first_name, ' ', last_name)) as 'Actor_Name'
